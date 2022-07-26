1fec5fd94cbd814964063e40480ca7c59760a5d8
waleedmalik7 <waleedmalik027@gmail.com>
2022-07-26 17:52:19 -0400

    efficient implementation using hash map

--- src/two_sum.java
+++ src/two_sum.java
@@ -1,15 +1,19 @@
+import java.util.HashMap;
+
 class two_sum {
     public int[] twoSum(int[] nums, int target) {
-        
-        int arr[] = new int[2];
+        int ans[] = new int[2];
+        HashMap<Integer, Integer> new_map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-            for(int j = 0; j <nums.length; j++){
-                if(nums[i]+nums[j]==target && i != j){
-                    arr[0] = i;
-                    arr[1] = j;
-                }
+            int val = target-nums[i];
+            if(new_map.containsKey(val)){
+                ans[0] = new_map.get(val);
+                ans[1] = i; 
+                break;
+            }else{
+                new_map.put(nums[i],i);
             }
-        }
-        return arr;
+        } 
+        return ans;
     }
 }
